97f9b04bdca75c2f1f7ac0c04e12c6e09cba8a92
Jakub Řičař <jakub.ricar@gmail.com>
2022-03-29 12:53:10 +0200

    Initial Commit

---
+++ Zradelnik/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zradelnik
+//
+//  Created by Jakub Řičař on 29.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zradelnik/ZradelnikApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZradelnikApp.swift
+//  Zradelnik
+//
+//  Created by Jakub Řičař on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ZradelnikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
