5a02c77e876e24d0a314ad87d6b1b9a65ff6f945
Jakub Řičař <jakub.ricar@gmail.com>
2022-04-03 22:42:16 +0200

    Apollo - use SQLite cache

--- Zradelnik/Network.swift
+++ Zradelnik/Network.swift
@@ -7,9 +7,33 @@
 
 import Foundation
 import Apollo
+import ApolloSQLite
 
 class Network {
     static let shared = Network()
     
-    private(set) lazy var apollo = ApolloClient(url: URL(string: "https://api-test.zradelnik.eu/graphql")!)
+    private(set) lazy var apollo = Network.createApollo()
+    
+    private static func createApollo() -> ApolloClient {
+        // 1. You'll have to figure out where to store your SQLite file.
+        // A reasonable place is the user's Documents directory in your sandbox.
+        // In any case, create a file URL for your file:
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let documentsURL = URL(fileURLWithPath: documentsPath)
+        let sqliteFileURL = documentsURL.appendingPathComponent("zradelnik_apollo_db.sqlite")
+        
+        // 2. Use that file URL to instantiate the SQLite cache:
+        let sqliteCache = try? SQLiteNormalizedCache(fileURL: sqliteFileURL)
+        
+        // 3. And then instantiate an instance of `ApolloStore` with the cache you've just created:
+        let store = ApolloStore(cache: sqliteCache ?? InMemoryNormalizedCache())
+        
+        let provider = DefaultInterceptorProvider(store: store)
+        let transport = RequestChainNetworkTransport(interceptorProvider: provider, endpointURL: URL(string: "https://api-test.zradelnik.eu/graphql")!)
+        
+        // 4. Assuming you've set up your `networkTransport` instance elsewhere,
+        // pass the store you just created into your `ApolloClient` initializer,
+        // and you're now set up to use the SQLite cache for persistent storage
+        return ApolloClient(networkTransport: transport, store: store)
+    }
 }
