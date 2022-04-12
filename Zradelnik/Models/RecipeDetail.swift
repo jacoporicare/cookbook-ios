3fdb45201d26313f171fc2ea1d11cb3d0b0ec8a7
Jakub Řičař <jakub.ricar@gmail.com>
2022-04-12 21:31:28 +0200

    Recipe detail - format time

--- Zradelnik/Models/RecipeDetail.swift
+++ Zradelnik/Models/RecipeDetail.swift
@@ -33,7 +33,7 @@ extension RecipeDetail {
         self.fullImageUrl = recipe.fullImageUrl
         self.directions = recipe.directions
         self.sideDish = recipe.sideDish
-        self.preparationTime = recipe.preparationTime?.formatted()
+        self.preparationTime = recipe.preparationTime?.formattedTime()
         self.servingCount = recipe.servingCount?.formatted()
         self.ingredients = recipe.ingredients?.map { Ingredient(from: $0) } ?? []
     }
@@ -44,7 +44,24 @@ extension RecipeDetail.Ingredient {
         self.id = ingredient.id
         self.name = ingredient.name
         self.isGroup = ingredient.isGroup
-        self.amount = ingredient.amount?.formatted(.number)
+        self.amount = ingredient.amount?.formatted()
         self.amountUnit = ingredient.amountUnit
+    }
+}
+
+private extension Int {
+    func formattedTime() -> String {
+        let hours = Int(Double(self) / 60.0)
+        let minutes = self % 60
+
+        if hours > 0, minutes == 0 {
+            return "\(hours) h"
+        }
+
+        if hours > 0, minutes > 0 {
+            return "\(hours) h \(minutes) min"
+        }
+
+        return "\(minutes) min"
     }
 }
