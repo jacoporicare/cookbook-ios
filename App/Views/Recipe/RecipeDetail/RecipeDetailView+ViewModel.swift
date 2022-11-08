b161b3cb22235edd2250d6add89da9168244fe5e
Jakub Řičař <jakub.ricar@gmail.com>
2022-11-08 21:28:31 +0100

    Cooked history - send UTC midnight

--- App/Views/Recipe/RecipeDetail/RecipeDetailView+ViewModel.swift
+++ App/Views/Recipe/RecipeDetail/RecipeDetailView+ViewModel.swift
@@ -11,15 +11,22 @@ import Foundation
 extension RecipeDetailView {
     class ViewModel: ObservableObject {
         @Published var isSaving: Bool = false
-        
+
+        private let UTCCalendar = {
+            var cal = Calendar(identifier: .gregorian)
+            cal.timeZone = .gmt
+            return cal
+        }()
+
         func recipeCooked(id: String, date: Date, completionHandler: @escaping (Recipe?) -> Void) {
             isSaving = true
-            
-            let mutation = RecipeCookedMutation(id: id, date: date)
-            
+
+            let dateUTCMidnight = UTCCalendar.startOfDay(for: date)
+            let mutation = RecipeCookedMutation(id: id, date: dateUTCMidnight)
+
             Network.shared.apollo.perform(mutation: mutation) { [weak self] result in
                 self?.isSaving = false
-                
+
                 switch result {
                 case .success(let result):
                     guard let recipe = result.data?.recipeCooked else { fallthrough }
